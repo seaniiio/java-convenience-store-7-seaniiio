3b25a533848ded05548f187c068de70f4def868d
seaniiio <cnc4750@naver.com>
2024-11-07 19:18:42 +0900

    refactor(Purchase): 재고 부족 시 출력하는 에러 메시지 수정

--- src/main/java/store/domain/Purchase.java
+++ src/main/java/store/domain/Purchase.java
@@ -22,7 +22,7 @@ public class Purchase {
 
         Product product = Products.getProduct(productName);
         if (!product.canBuy(quantity)) {
-            throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
         }
     }
 
