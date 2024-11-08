03c0d18584df46b31de54cea9a8766b3a8c5e01f
seaniiio <cnc4750@naver.com>
2024-11-08 15:58:14 +0900

    refactor(PurchaseService): 사용하지 않는 객체를 필드에서 제거

--- src/main/java/store/service/PurchaseService.java
+++ src/main/java/store/service/PurchaseService.java
@@ -3,15 +3,12 @@ package store.service;
 import java.util.List;
 import java.util.Map;
 import store.domain.Purchases;
-import store.util.InputFormatter;
 
 public class PurchaseService {
 
-    private final InputFormatter inputFormatter;
     private final Purchases purchases;
 
     public PurchaseService() {
-        this.inputFormatter = new InputFormatter();
         this.purchases = new Purchases();
     }
 
