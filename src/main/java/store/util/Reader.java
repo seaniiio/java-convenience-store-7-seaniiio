39ccd60f5a9c9692f57b0a4fdb739de1c1c5644f
seaniiio <cnc4750@naver.com>
2024-11-10 16:35:20 +0900

    refactor(Reader): UTF-8 인코딩

--- src/main/java/store/util/Reader.java
+++ src/main/java/store/util/Reader.java
@@ -1,8 +1,11 @@
 package store.util;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +25,7 @@ public class Reader {
     private static List<String> readFile(String filePath) {
         List<String> contents = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), StandardCharsets.UTF_8))) {
             String line;
             boolean isFirstLine = true;
 
